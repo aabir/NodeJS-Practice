da126c4ab0f1631b9eaa303351892021e2abe8bb
Shible Noman <Shible.Noman@sage.com>
2021-01-17 21:15:58 +0800

    Update lowDB

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
 const config = {
   ENDPOINT: '/api',
   getDB: ()=> {
-    const low = require('lowdb')
-    const FileSync = require('lowdb/adapters/FileSync')
-    const adapter = new FileSync('db.json')
-    const db = low(adapter)
-
     db.defaults({ products: []}).write();
     db.defaults({users: []}).write();
     return db;
